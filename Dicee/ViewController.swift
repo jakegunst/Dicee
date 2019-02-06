662541b37e8c9aea0505cf3e229b6aad3013bf67
Jake Gunst <jacobwgunst@gmail.com>
2019-02-06 15:24:21 -0600

    Add dice array, developed cheating function.

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
     var randomDiceIndex1: Int = 0
     var randomDiceIndex2: Int = 0
     
@@ -23,6 +25,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
 
@@ -31,9 +35,12 @@ class ViewController: UIViewController {
         randomDiceIndex1 = Int.random(in: 0 ... 5)
         randomDiceIndex2 = Int.random(in: 0 ... 5)
         
-        
-        
         //This will get exucuted when the roll button gets pressed.
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+    
         
         //buttons have outlets AND actions
         
@@ -44,6 +51,10 @@ class ViewController: UIViewController {
         
         randomDiceIndex1 = Int.random(in: 5 ... 5)
         randomDiceIndex2 = Int.random(in: 5 ... 5)
+        
+        diceImageView1.image = UIImage(named: "dice6")
+        
+        diceImageView2.image = UIImage(named: "dice6")
         
     }
 }
