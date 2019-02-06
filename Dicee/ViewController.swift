213289eef5eea94042f89c0b8ae51f675e6d7f8c
Jake Gunst <jacobwgunst@gmail.com>
2019-02-05 20:57:58 -0600

    Began adding functionality to button and images (random number generation)

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    //image views only have outlets
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        randomDiceIndex1 = Int.random(in: 0 ... 5)
+        randomDiceIndex2 = Int.random(in: 0 ... 5)
+        
+        
+        
+        //This will get exucuted when the roll button gets pressed.
+        
+        //buttons have outlets AND actions
+        
+    }
+    @IBAction func rollButtonDragged(_ sender: Any) {
+        
+        //This will get exucuted when the roll button gets pressed.
+        
+        randomDiceIndex1 = Int.random(in: 5 ... 5)
+        randomDiceIndex2 = Int.random(in: 5 ... 5)
+        
+    }
 }
 
